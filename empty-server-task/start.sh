ca39cabcce8d3abd27b59110e6b577dac87c6144
edgar-arzakantsyan-sada <edgar.arzakantsyan@sada.com>
2025-09-08 13:00:03 +0400

    New Commit Fixed an issue

--- empty-server-task/start.sh
+++ empty-server-task/start.sh
@@ -86,7 +86,7 @@ echo "Installing Node Exporter for future Prometheus Integration ... "
 
 /usr/bin/wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.8.2.linux-amd64.tar.gz
-mv node_exporter-1.8.2.linux-amd64/node_exportetr /usr/local/bin/
+mv node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin/
 echo "Installed ! "
 
 echo "Creating a system user called node_exporter ..."
